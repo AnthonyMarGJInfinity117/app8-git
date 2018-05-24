fd97100ae555f1c28a046c3998d2ed012f3e5b62
Ronaldo <Ronaldo>
2018-05-23 19:50:03 -0500

    4subida

--- js/acciones2.js
+++ js/acciones2.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 $(document).ready(function(e) {
     document.addEventListener("deviceready", function(){
-		$('#sonar').tap(function(){
-			navigator.notification.beep(2);
-        });//cierre de sonar
+	$('#sonar').tap(function(){
+		navigator.notification.beep(2);
+        }); //Cierre de sonar
         $('#vibrar').tap(function(){
 	        navigator.notification.vibrate(2000);
-        });//cierre de vibrar
-	},false);//cierre de deviceready
-});//cierre ready
+        }); //cierre de vibrar
+      },false); //cierre de deviceready
+}); //cierre ready
 	
 
